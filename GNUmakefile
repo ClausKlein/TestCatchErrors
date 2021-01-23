7873966eac1666e9b981bc60e2a95bac1e8ae2ad
ClausKlein <claus.klein@arcormail.de>
2021-01-23 19:37:36 +0100

    do not longer use folly code
    we use OnLeavingScope to makeScopeGuard (needs c++17 or newer!)

--- GNUmakefile
+++ GNUmakefile
@@ -1,9 +1,9 @@
 LDFLAGS=-L/usr/local/lib
 LDLIBS:=-lboost_filesystem
-CXXFLAGS:=-std=c++2a -Wextra
+CXXFLAGS:=-std=c++17 -Wextra
 CPPFLAGS:=-MMD
 
-PROGRAMMS:= ScopeGuardTest UncaughtExceptionCounter ScopeGuardOnExit uncaught_exception src/main
+PROGRAMMS:= OnLeavingScope ScopeGuardTest UncaughtExceptionCounter ScopeGuardOnExit uncaught_exception src/main
 
 SRC:=$(PROGRAMMS:=.cpp)
 SRC+= src/ErrorHandler.cpp
@@ -12,19 +12,21 @@ OBJ:=$(SRC:.cpp=.o)
 DEP:=$(OBJ:.o=.d)
 
 .INTERMEDIATE: $(OBJ)
-.PHONY: all check clean
+.PHONY: all test check clean
 all: $(PROGRAMMS)
 
 src/main: src/main.o src/ErrorHandler.o
 	$(LINK.cc) $< src/ErrorHandler.o -o $@
 
-ScopeGuardTest: LDLIBS:=-lfolly -lgtest -lglog -lgtest_main
+ScopeGuardTest: LDLIBS:=-lgtest -lglog -lgtest_main
 ScopeGuardTest: ScopeGuardTest.cpp
 	$(LINK.cc) $(LDLIBS) $< -o $@
 
-
 %: %.cpp
 	$(LINK.cc) $(LDLIBS) $< -o $@
+
+test: ScopeGuardTest
+	./$<
 
 check:
 	clang-tidy *.hpp
---
+++ OnLeavingScope.cpp
@@ -0,0 +1,101 @@
+// from
+// https://crascit.com/2015/06/03/on-leaving-scope-part-2/#more-226
+//
+
+#include <functional>
+#include <iostream>
+#include <string>
+
+namespace {
+#if 1
+template <typename Func> class OnLeavingScope
+{
+public:
+    // Prevent copying
+    OnLeavingScope(const OnLeavingScope&) = delete;
+    OnLeavingScope& operator=(const OnLeavingScope&) = delete;
+
+    OnLeavingScope(const Func& f) : m_func(f) {}
+    OnLeavingScope(Func&& f) noexcept : m_func(std::move(f)) {}
+
+    ~OnLeavingScope() { m_func(); }
+
+private:
+    Func m_func;
+};
+
+#else
+
+class OnLeavingScope
+{
+public:
+    // Use std::function so we can support
+    // any function-like object
+    using Func = std::function<void()>;
+
+    // Prevent copying
+    OnLeavingScope(const OnLeavingScope&) = delete;
+    OnLeavingScope& operator=(const OnLeavingScope&) = delete;
+
+    OnLeavingScope(const Func& f) : m_func(f) {}
+    OnLeavingScope(Func&& f) noexcept : m_func(std::move(f)) {}
+
+    ~OnLeavingScope() { m_func(); }
+
+private:
+    Func m_func;
+};
+#endif
+
+class Printer
+{
+public:
+    Printer(const std::string& msg) : m_msg(msg) { std::cout << "Created" << std::endl; }
+
+    Printer(const Printer& p) : m_msg(p.m_msg) { std::cout << "Copied" << std::endl; }
+
+    Printer(Printer&& p) : m_msg(std::move(p.m_msg)) { std::cout << "Moved" << std::endl; }
+
+    void operator()() const { std::cout << m_msg << std::endl; }
+
+private:
+    std::string m_msg;
+};
+
+// Note non-void return type to demonstrate
+// we are not restricted to void, unlike
+// the std::function approach which is.
+bool freeFunc() noexcept
+{
+    std::cout << "free function" << std::endl;
+    return true;
+}
+
+void tester()
+{
+    const auto p = Printer("lvalue");
+    auto olsLValue = OnLeavingScope(p);
+    std::cout << "Printer lvalue size = " << sizeof(olsLValue) << std::endl;
+
+    std::cout << "----" << std::endl;
+    const auto olsRValue = OnLeavingScope(Printer("rvalue"));
+    std::cout << "Printer rvalue size = " << sizeof(olsRValue) << std::endl;
+
+    std::cout << "----" << std::endl;
+    auto olsLambda = OnLeavingScope([] { std::cout << "lambda" << std::endl; });
+    std::cout << "Lambda size = " << sizeof(olsLambda) << std::endl;
+
+    std::cout << "----" << std::endl;
+    auto olsFreeFunc = OnLeavingScope(&freeFunc);
+    std::cout << "Free function size = " << sizeof(olsFreeFunc) << std::endl;
+
+    std::cout << "----" << std::endl;
+}
+} // namespace
+
+int main()
+{
+    tester();
+
+    auto cleanup = OnLeavingScope([] { std::cout << "Good by" << std::endl; });
+}
